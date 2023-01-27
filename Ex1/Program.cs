d76e7fe72a4baa324dd0ddd0f61b09af8e73ee3b
Alexandr <dajj2012@mail.ru>
2023-01-27 11:56:07 +0300

    2

--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void InputArray(string?[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] =Console.ReadLine(); 
+}
+
+void exitArray(string?[] array, int n)
+{
+    for (int i=0; i<array.Length; i++)
+        if (array[i].Length<=n)
+            Console.WriteLine(array[i]);
+}
+
+Console.Clear();
+Console.WriteLine("Введите длинну масива ");
+int n =  int.Parse(Console.ReadLine());
+string?[] array = new string[n];
+InputArray(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+Console.WriteLine("Введите длину искамых строк ");
+int m =  int.Parse(Console.ReadLine());
+exitArray(array,m);
